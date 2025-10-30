0e205bd0d496e7d14cf867625dae5f2ea388f5d4
Josh Grosse <josh@jggimi.net>
2025-10-29 21:45:19 -0400

    revert websocketpp to 0.5.0 for OpenBSD

--- src/third_party/websocketpp/websocketpp/common/type_traits.hpp
+++ src/third_party/websocketpp/websocketpp/common/type_traits.hpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014, Peter Thorson. All rights reserved.
+ * Copyright (c) 2015, Peter Thorson. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -25,35 +25,41 @@
  *
  */
 
-#ifndef WEBSOCKETPP_COMMON_REGEX_HPP
-#define WEBSOCKETPP_COMMON_REGEX_HPP
+#ifndef WEBSOCKETPP_COMMON_TYPE_TRAITS_HPP
+#define WEBSOCKETPP_COMMON_TYPE_TRAITS_HPP
 
-#if defined _WEBSOCKETPP_CPP11_STL_ && !defined _WEBSOCKETPP_NO_CPP11_REGEX_
-    #ifndef _WEBSOCKETPP_CPP11_REGEX_
-        #define _WEBSOCKETPP_CPP11_REGEX_
+#include <websocketpp/common/cpp11.hpp>
+
+// If we've determined that we're in full C++11 mode and the user hasn't
+// explicitly disabled the use of C++11 functional header, then prefer it to
+// boost.
+#if defined _WEBSOCKETPP_CPP11_INTERNAL_ && !defined _WEBSOCKETPP_NO_CPP11_TYPE_TRAITS_
+    #ifndef _WEBSOCKETPP_CPP11_TYPE_TRAITS_
+        #define _WEBSOCKETPP_CPP11_TYPE_TRAITS_
     #endif
 #endif
 
-#ifdef _WEBSOCKETPP_CPP11_REGEX_
-    #include <regex>
+
+#ifdef _WEBSOCKETPP_CPP11_TYPE_TRAITS_
+    #include <type_traits>
 #else
-    #include <boost/regex.hpp>
+    #include <boost/aligned_storage.hpp>
 #endif
+
+
 
 namespace websocketpp {
 namespace lib {
 
-#ifdef _WEBSOCKETPP_CPP11_REGEX_
-    using std::cmatch;
-    using std::regex;
-    using std::regex_match;
+#ifdef _WEBSOCKETPP_CPP11_TYPE_TRAITS_
+    using std::aligned_storage;
+    using std::is_same;
 #else
-    using boost::cmatch;
-    using boost::regex;
-    using boost::regex_match;
+    using boost::aligned_storage;
+    using boost::is_same;
 #endif
 
 } // namespace lib
 } // namespace websocketpp
 
-#endif // WEBSOCKETPP_COMMON_REGEX_HPP
+#endif // WEBSOCKETPP_COMMON_TYPE_TRAITS_HPP
